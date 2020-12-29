a86ca7c9a9d1adeb1928b6e55a88c8575ca22c7c
billjjjj <novicetown@gmail.com>
2020-12-29 12:34:09 +0800

    global variable, push notification added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SnackbarProvider } from 'notistack';
 import './App.css';
 import { ThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,13 +8,27 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Routes from './Routes';
 // mui themes
 import theme from './theme';
+// Global variable
+window.siteSetting = {
+  siteName: 'HK Covid-19 APP',
+  copyRight: '© 302CEM The First Team',
+  apiUrl: 'http://localhost:5000',
+};
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <Router>
-        <Routes />
-      </Router>
+      <SnackbarProvider
+        maxSnack={3}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'right',
+        }}
+      >
+        <Router>
+          <Routes />
+        </Router>
+      </SnackbarProvider>
     </ThemeProvider>
   );
 };
--- src/hooks/https/useDelete.js
+++ src/hooks/https/useDelete.js
@@ -1,18 +1,26 @@
 import { useState, useCallback } from 'react';
 import axios from 'axios';
+import { useSnackbar } from 'notistack';
 
 const useDelete = ({ url, headers }) => {
   const [res, setRes] = useState({ data: null, isLoading: false, error: null });
+  const { enqueueSnackbar } = useSnackbar();
 
   const callAPI = useCallback(() => {
     setRes((prevState) => ({ ...prevState, isLoading: true }));
     axios
-      .delete(process.env.REACT_APP_APIURL + url)
+      .delete(window.siteSetting.apiUrl + url)
       .then((response) => {
         setRes({ data: response.data, isLoading: false, error: null });
+        enqueueSnackbar('Added successful', {
+          variant: 'success',
+        });
       })
       .catch((error) => {
         setRes({ data: null, isLoading: false, error });
+        enqueueSnackbar('Added Error', {
+          variant: 'error',
+        });
       });
   }, [url, headers]);
   return [res, callAPI];
