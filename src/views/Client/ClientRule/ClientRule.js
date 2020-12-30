29ac82977a0b8ca9840a5be9a35851767c022bf8
thomastam73 <thomastam73@gmail.com>
2020-12-30 16:53:43 +0800

    update

--- src/views/Client/ClientRule/ClientRule.js
+++ src/views/Client/ClientRule/ClientRule.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { CircularProgress, Grid } from '@material-ui/core';
+// hook
+import { useFetch } from '../../../hooks';
+// component
+import RuleCard from './CardRule';
 
 const ClientRule = () => {
-  return <div>ClientRule.js</div>;
+  const { isLoading } = useFetch('/rules');
+
+  return (
+    <div>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={4}>
+          <RuleCard />
+        </Grid>
+      )}
+    </div>
+  );
 };
 
 export default ClientRule;
