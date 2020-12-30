eb062e2258aec2a60c7e072f5cc6a0294cde6d19
thomastam73 <thomastam73@gmail.com>
2020-12-30 14:52:36 +0800

    show case ui done

--- src/views/Client/ClientCase/ClientCase.js
+++ src/views/Client/ClientCase/ClientCase.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { CircularProgress, Grid } from '@material-ui/core';
+// hook
+import { useFetch } from '../../../hooks';
+// component
+import CaseCard from './CaseCard';
 
 const ClientCase = () => {
-  return <div>ClientCase.js</div>;
+  const { isLoading } = useFetch('/cases');
+
+  return (
+    <div>
+      {isLoading ? (
+        <CircularProgress />
+      ) : (
+        <Grid container spacing={4}>
+          <CaseCard />
+        </Grid>
+      )}
+    </div>
+  );
 };
 
 export default ClientCase;
