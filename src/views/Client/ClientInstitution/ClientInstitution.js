707451c9fe41b7efa1d1235267fd68dead486fcb
thomastam73 <thomastam73@gmail.com>
2021-01-04 03:54:19 +0800

    update institution

--- src/views/Client/ClientInstitution/ClientInstitution.js
+++ src/views/Client/ClientInstitution/ClientInstitution.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
 import { CircularProgress, Grid } from '@material-ui/core';
 // hook
 import { useFetch } from '../../../hooks';
 // component
 import InstitutionCard from './CardInstitution';
+// img
+import img1 from './img1.png';
+import img2 from './img2.png';
 
 const ClientInstitution = () => {
   const { isLoading } = useFetch('/institutions');
@@ -13,7 +17,24 @@ const ClientInstitution = () => {
       {isLoading ? (
         <CircularProgress />
       ) : (
-        <Grid container spacing={4}>
+        <Grid container alignItems="center" justify="center" spacing={1}>
+          <Grid item xs={5}>
+            <img src={img1} alt="img1" width="450" />
+          </Grid>
+          <Grid item xs={5}>
+            <img src={img2} alt="img2" width="450" />
+          </Grid>
+          <Grid item xs={4}>
+            <Button
+              target="_blank"
+              href="https://booking.communitytest.gov.hk/form/index.jsp"
+              variant="outlined"
+              color="secondary"
+              width="600"
+            >
+              Community Testing Centre - Booking System
+            </Button>
+          </Grid>
           <InstitutionCard />
         </Grid>
       )}
